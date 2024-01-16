ecc3650d315255b7206a67186501f38b859fb4cc
X9 <david.agu@iisysgroup.com>
2024-01-16 16:12:31 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import Axios from "axios";
+import './App.css';
+import { FaSearch } from "react-icons/fa";
+import {FcSpeaker} from "react-icons/fc";
+
+function App() {
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
